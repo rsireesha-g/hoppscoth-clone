fe9235645c032854c69fb7dcd94ce1e4118ca043
R Sireesha <92039117+rsiree@users.noreply.github.com>
2025-07-10 21:01:27 +0530

    page: websocket page static ui

--- src/pages/realtime.tsx
+++ src/pages/realtime.tsx
@@ -1,11 +1,191 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Layout } from '../components/layout'
 import { TopNavigation } from '../components/realtimeComponents/topNavigation'
+import { Button } from '../components/button'
+import { BsArrow90DegLeft, BsArrowDown, BsArrowUp, BsCheck, BsChevronDown, BsCopy, BsInfoCircle } from 'react-icons/bs'
+import { Tooltip } from '../components/tooltip'
+import { MdOutlineDeleteForever, MdOutlineDeleteOutline, MdOutlineWrapText, MdSend } from 'react-icons/md'
+import DropdownMenu from '../components/dropdownMenu'
+import { Link } from 'react-router'
+import { CiCircleQuestion } from 'react-icons/ci'
+import { PiMagicWand } from 'react-icons/pi'
+import { BiSolidFileImport } from 'react-icons/bi'
+import { IoAdd } from 'react-icons/io5';
+import emptyImage from "../assests/images/environment.png"
 
 export const Realtime = () => {
+    const [isConnected, setIsConnected] = useState(-1);
+    const [tab, setTab] = useState(1);
+    const [messageType, setMessageType] = useState('JSON');
+
     return (
         <Layout page='realtime'>
             <TopNavigation currPage='web' />
+            <div className="w-full p-4 ">
+                <div className="flex gap-2 justify-between">
+                    <input type="text" className='w-full flex-grow bg-primaryDark p-2' value='wss://echo-websocket.hoppscotch.io' />
+                    <Button type='primary' text='Connect' onClick={() => setIsConnected(isConnected === -1 ? 0 : isConnected === 0 ? 1 : 0)} />
+                </div>
+
+                <div className=''>
+                    <div className="flex justify-start gap-4 align-middle p-2">
+                        <div onClick={() => setTab(1)}
+                            className={`${tab === 1 && 'border-b-2 border-b-accentDark !text-secondaryDark'} cursor-pointer text-secondaryLight hover:text-secondaryDark p-2 pl-0`}>
+                            Communications
+                        </div>
+                        <div onClick={() => setTab(2)}
+                            className={`${tab === 2 && 'border-b-2 border-b-accentDark !text-secondaryDark'} cursor-pointer text-secondaryLight hover:text-secondaryDark p-2 pl-0`}>
+                            Protocols
+                        </div>
+                    </div>
+                    {tab === 1 ?
+                        <>
+                            <div className="flex justify-between gap-4 align-middle">
+                                <div className="flex gap-2 align-middle items-center">
+                                    <p className="text-secondaryLight text-[10px]">Message</p>
+                                    <DropdownMenu
+                                        position='bottom-left'
+                                        button={
+                                            <Button type='secondary' text='JSON' extraClass='!flex-row border-0' chevronExists={true} />
+                                        }
+                                    >
+                                        <div className="flex gap-4" onClick={() => setMessageType("JSON")}>
+                                            <p>JSON</p>
+                                            {messageType === 'JSON' && <BsCheck size={16} className='text-accentDark' />}
+                                        </div>
+                                        <div className="flex gap-4" onClick={() => setMessageType("Raw")}>
+                                            <p>Raw</p>
+                                            {messageType === 'Raw' && <BsCheck size={16} className='text-accentDark' />}
+                                        </div>
+                                    </DropdownMenu>
+                                </div>
+                                <div className="flex gap-4 align-middle items-center">
+                                    <div className='cursor-not-allowed text-accentDark flex gap-2'>
+                                        <MdSend size={16} />
+                                        <p >Send</p>
+                                    </div>
+                                    <Tooltip position='top-left' text='Clear input on send'>
+                                        <input type='checkbox' />
+                                        <label>Clear Input</label>
+                                    </Tooltip>
+                                    <Tooltip position='top-left' text='Wiki'>
+                                        <Link target='_blank' to='https://docs.hoppscotch.io/documentation/features/realtime-api-testing' >
+                                            <CiCircleQuestion size={16} className='cursor-pointer' />
+                                        </Link>
+                                    </Tooltip>
+                                    <Tooltip position='top-left' text='Clear All'>
+                                        <MdOutlineDeleteForever size={16} className='cursor-pointer' />
+                                    </Tooltip>
+                                    <Tooltip position='top-left' text='Wrap lines'>
+                                        < MdOutlineWrapText size={16} className='cursor-pointer' />
+                                    </Tooltip>
+                                    <Tooltip position='top-right' text='Prettify'>
+                                        <PiMagicWand size={16} className='cursor-pointer' />
+                                    </Tooltip>
+                                    <Tooltip position='top-right' text='Import'>
+                                        <BiSolidFileImport size={16} className='cursor-pointer' />
+                                    </Tooltip>
+                                </div>
+                            </div>
+                            <div className="flex gap-2 align-middle items-center min-h-30">
+                                <div className="flex">1</div>
+                                <input type="text" className='text-accentDark' placeholder='Message' />
+                            </div>
+                        </>
+                        :
+                        <>
+                            <div className="flex justify-between gap-4 align-middle">
+                                <div className="flex gap-2 align-middle items-center">
+                                    <p className="text-secondaryLight text-[10px]">Protocols</p>
+                                </div>
+                                <div className="flex gap-4 align-middle items-center">
+                                    <Tooltip position='top-left' text='Clear All'>
+                                        <MdOutlineDeleteForever size={16} className='cursor-pointer' />
+                                    </Tooltip>
+                                    <Tooltip position='top-left' text='Wrap lines'>
+                                        <IoAdd size={16} className='cursor-pointer' />
+                                    </Tooltip>
+                                </div>
+                            </div>
+                            <div className="flex gap-2 align-middle items-center min-h-30 flex-col ">
+                                <img src={emptyImage} alt="no protocols" width={100} height={100} />
+                                <p className='text-[10px] text-secondaryLight'>Protocols are empty</p>
+                            </div>
+                        </>
+                    }
+                </div>
+
+                {isConnected !== -1 &&
+                    <div className='my-4'>
+                        <div className="flex justify-between p-2 border-y border-y-dividerDark">
+                            <p className="text-secondary text-[10px]">Log</p>
+                            <div className="flex gap-2">
+                                <Tooltip position='top-right' text='Delete'>
+                                    <MdOutlineDeleteOutline size={14} />
+                                </Tooltip>
+                                <Tooltip position='top-right' text='Scroll up'>
+                                    <BsArrowUp size={14} />
+                                </Tooltip>
+                                <Tooltip position='top-right' text='Scroll down'>
+                                    <BsArrowDown size={14} />
+                                </Tooltip>
+                                <Tooltip position='top-right' text='Autoscroll: Turn off'>
+                                    <BsChevronDown size={14} />
+                                </Tooltip>
+
+                            </div>
+                        </div>
+                        <>
+                            <div className="flex justify-between border-b border-b-dividerDark px-2">
+                                <div className="flex justify-start gap-2 w-full">
+                                    <BsInfoCircle size={14} className='text-getColor mt-2' />
+                                    <p className='text-secondaryLight text-[9px] border-x border-x-dividerDark px-4 py-2'>Jul 11, 2025, 7:20:20 AM</p>
+                                    <div className='flex-grow flex justify-between gap-4 py-2 '>
+                                        <p className='text-secondaryLight  flex-grow'>Connected to wss://echo-websocket.hoppscotch.io</p>
+                                        <div className='flex gap-4'>
+                                            <BsCopy size={16} className='opacity-0 hover:opacity-10' />
+                                            <BsChevronDown size={16} />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex justify-between border-b border-b-dividerDark px-2">
+                                <div className="flex justify-start gap-2 w-full">
+                                    <BsArrowDown size={14} className='text-blue-400 mt-2 rotate-45' />
+                                    <p className='text-secondaryLight text-[9px] border-x border-x-dividerDark px-4 py-2'>Jul 11, 2025, 7:20:20 AM</p>
+                                    <div className='flex-grow flex justify-between gap-4 py-2 '>
+                                        <p className='text-secondaryLight  flex-grow'>14:22:13 GMT+0000 (Coordinated Universal Time)</p>
+                                        <div className='flex gap-4'>
+                                            <BsCopy size={16} className='opacity-0 hover:opacity-10' />
+                                            <BsChevronDown size={16} />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+
+                        {
+                            isConnected === 1 &&
+                            <>
+                                <div className="flex justify-between border-b border-b-dividerDark px-2">
+                                    <div className="flex justify-start gap-2 w-full">
+                                        <BsInfoCircle size={14} className='text-deleteColor mt-2' />
+                                        <p className='text-secondaryLight text-[9px] border-x border-x-dividerDark px-4 py-2'>Jul 11, 2025, 7:20:20 AM</p>
+                                        <div className='flex-grow flex justify-between gap-4 py-2 '>
+                                            <p className='text-secondaryLight  flex-grow'>Disconnected from wss://echo-websocket.hoppscotch.io</p>
+                                            <div className='flex gap-4'>
+                                                <BsCopy size={16} className='opacity-0 hover:opacity-10' />
+                                                <BsChevronDown size={16} />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </>
+                        }
+                    </div>
+                }
+
+            </div>
         </Layout>
     )
 }
